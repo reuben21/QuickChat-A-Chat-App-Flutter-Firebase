0f513fd7746a63b79083575d8471e69faf87c7bd
reuben21 <reuben211999@icloud.com>
2021-06-08 19:25:43 +0530

    Updated Bubble

--- lib/widget/chat/message_bubble.dart
+++ lib/widget/chat/message_bubble.dart
@@ -1,16 +1,11 @@
-import 'package:bubble/bubble.dart';
+
+import 'package:flutter/cupertino.dart';
 import '../../colors.dart';
 import 'package:flutter/material.dart';
 
 class MessageBubble extends StatelessWidget {
-  MessageBubble(
-    this.message,
-    this.id,
-    this.username,
-    this.imageUrl,
-    this.isMe,
-      this.index
-  );
+  MessageBubble(this.message, this.id, this.username, this.imageUrl, this.isMe,
+      this.index);
 
   final int id;
   final String message;
@@ -22,148 +17,209 @@ class MessageBubble extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if (id == 10) {
-      return Container(
-        margin: EdgeInsets.all(10),
-        child: Bubble(
-          alignment: Alignment.center,
-          color: kPrimaryColorAccent,
-          child: Text('${username} Entered This Chat',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 15.0, color: kPrimaryColor)),
-        ),
+      return Column( mainAxisSize: MainAxisSize.min,
+
+        children: [
+          Container(
+
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(9),
+            decoration: BoxDecoration(
+                color: kPrimaryColorAccent,
+                borderRadius: BorderRadius.all(Radius.circular(10))),
+            child: Text('${username} Entered This Chat',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 15.0, color: kPrimaryColor)),
+            ),
+        ],
       );
     }
     if (id == 1) {
-      return Container(
-        margin: EdgeInsets.all(10),
-        child: Bubble(
-          alignment: Alignment.center,
-          color: kPrimaryColorAccent,
-          child: Text('${username} Created This Chat',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 15.0, color: kPrimaryColor)),
-        ),
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(9),
+            decoration: BoxDecoration(
+                color: kPrimaryColorAccent,
+                borderRadius: BorderRadius.all(Radius.circular(10))),
+            child: Text('${username} Created This Chat',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 15.0, color: kPrimaryColor)),
+            ),
+        ],
       );
     }
     if (id == 2) {
-      return Container(
-        margin: EdgeInsets.all(10),
-        child: Bubble(
-          alignment: Alignment.center,
-          color: kPrimaryColorAccent,
-          child: Text('${username} ${message}',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 15.0, color: kPrimaryColor)),
-        ),
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(9),
+            decoration: BoxDecoration(
+                color: kPrimaryColorAccent,
+                borderRadius: BorderRadius.all(Radius.circular(10))),
+            child: Text('${username} ${message}',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 15.0, color: kPrimaryColor)),
+            ),
+        ],
       );
     }
     if (id == 5) {
-      return Bubble(
-        margin: isMe
-            ? BubbleEdges.only(top: 10, left: 40)
-            : BubbleEdges.only(top: 10, right: 40),
-        alignment: isMe ? Alignment.topRight : Alignment.topLeft,
-        nip: isMe ? BubbleNip.rightTop : BubbleNip.leftTop,
-        color: isMe ? senderMessageColor : receiverMessageColor,
-        child: Column(
-          crossAxisAlignment:
-              isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-          children: [
-            Text(
-              message,
-              textAlign: isMe ? TextAlign.end : TextAlign.start,
-              style: isMe
-                  ? Theme.of(context).textTheme.bodyText1
-                  : Theme.of(context).textTheme.bodyText2,
-            ),
-            Text(
-              username,
-              style: Theme.of(context).textTheme.headline5,
-              textAlign: isMe ? TextAlign.end : TextAlign.start,
-            )
-          ],
-        ),
-      );
-    }
-    if (id == 6) {
-      return Bubble(
-        margin: isMe
-            ? BubbleEdges.only(top: 10, left: 40)
-            : BubbleEdges.only(top: 10, right: 40),
-        alignment: isMe ? Alignment.topRight : Alignment.topLeft,
-        nip: isMe ? BubbleNip.rightTop : BubbleNip.leftTop,
-        color: isMe ? senderMessageColor : receiverMessageColor,
-        child: Column(
-          crossAxisAlignment:
-              isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-          children: [
-            Container(
-              width: 200,
-              height: 200,
-              decoration: BoxDecoration(
-                  color: kPrimaryColorAccent,
-                  borderRadius: BorderRadius.all(Radius.circular(10))),
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (_) {
-                    return DetailScreen(index,imageUrl);
-                  }));
-                },
-                child: Hero(
-                  tag: index.toString(),
-                  child: ClipRRect(
-                    borderRadius:BorderRadius.all(Radius.circular(5)) ,
-                    child: FadeInImage(
-                      placeholder: NetworkImage(
-                          'https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif'),
-                      image: NetworkImage(imageUrl),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Text(
-              message,
-              textAlign: isMe ? TextAlign.end : TextAlign.start,
-              style: isMe
-                  ? Theme.of(context).textTheme.bodyText1
-                  : Theme.of(context).textTheme.bodyText2,
-            ),
-            Text(
-              username,
-              style: Theme.of(context).textTheme.headline5,
-              textAlign: isMe ? TextAlign.end : TextAlign.start,
-            )
-          ],
-        ),
-      );
-    }
-    return Bubble(
-      margin: isMe
-          ? BubbleEdges.only(top: 10, left: 40)
-          : BubbleEdges.only(top: 10, right: 40),
-      alignment: isMe ? Alignment.topRight : Alignment.topLeft,
-      nip: isMe ? BubbleNip.rightTop : BubbleNip.leftTop,
-      color: isMe ? senderMessageColor : receiverMessageColor,
-      child: Column(
+      return Column(
+        mainAxisSize: MainAxisSize.min,
         crossAxisAlignment:
             isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
         children: [
-          Text(
-            message,
-            textAlign: isMe ? TextAlign.end : TextAlign.start,
-            style: isMe
-                ? Theme.of(context).textTheme.bodyText1
-                : Theme.of(context).textTheme.bodyText2,
+          Container(
+            margin:isMe ? EdgeInsets.only(right: 5,top: 5,bottom: 5):EdgeInsets.only(left: 5,top: 5,bottom: 5),
+            padding: EdgeInsets.all(5),
+            decoration: BoxDecoration(
+                color: isMe ? senderMessageColor : receiverMessageColor,
+                borderRadius: isMe? BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                    bottomLeft: Radius.circular(10)) : BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                    bottomRight: Radius.circular(10))
+            ),
+            child: Column(
+              crossAxisAlignment:
+              isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  child: Text(
+                    message,
+                    textAlign: isMe ? TextAlign.end : TextAlign.start,
+                    style: isMe
+                        ? Theme.of(context).textTheme.bodyText1
+                        : Theme.of(context).textTheme.bodyText2,
+                  ),
+                ),
+                Text(
+                  username,
+                  style: Theme.of(context).textTheme.headline5,
+                  textAlign: isMe ? TextAlign.end : TextAlign.start,
+                )
+              ],
+            ),
           ),
-          Text(
-            username,
-            style: Theme.of(context).textTheme.headline5,
-            textAlign: isMe ? TextAlign.end : TextAlign.start,
-          )
         ],
-      ),
+      );
+    }
+    if (id == 6) {
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+
+        crossAxisAlignment:
+        isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin:isMe ? EdgeInsets.only(right: 5,top: 5,bottom: 5):EdgeInsets.only(left: 5,top: 5,bottom: 5),
+            padding: EdgeInsets.all(5),
+            decoration: BoxDecoration(
+                color: isMe ? senderMessageColor : receiverMessageColor,
+                borderRadius: isMe? BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                    bottomLeft: Radius.circular(10)) : BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                    bottomRight: Radius.circular(10))
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment:
+                  isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: 200,
+                  height: 200,
+
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (_) {
+                        return DetailScreen(index, imageUrl);
+                      }));
+                    },
+                    child: Hero(
+                      tag: index.toString(),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        child: FadeInImage(
+                          placeholder: NetworkImage(
+                              'https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif'),
+                          image: NetworkImage(imageUrl),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Text(
+                  message,
+                  textAlign: isMe ? TextAlign.end : TextAlign.start,
+                  style: isMe
+                      ? Theme.of(context).textTheme.bodyText1
+                      : Theme.of(context).textTheme.bodyText2,
+                ),
+                Text(
+                  username,
+                  style: Theme.of(context).textTheme.headline5,
+                  textAlign: isMe ? TextAlign.end : TextAlign.start,
+                )
+              ],
+            ),
+          ),
+        ],
+      );
+    }
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment:
+      isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+      children: [
+        Container(
+          margin:isMe ? EdgeInsets.only(right: 5,top: 5,bottom: 5):EdgeInsets.only(left: 5,top: 5,bottom: 5),
+          padding: EdgeInsets.all(5),
+          decoration: BoxDecoration(
+              color: isMe ? senderMessageColor : receiverMessageColor,
+              borderRadius: isMe? BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                  bottomLeft: Radius.circular(10)) : BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                  bottomRight: Radius.circular(10))
+          ),
+          child: Column(
+            crossAxisAlignment:
+            isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                child: Text(
+                  message,
+                  textAlign: isMe ? TextAlign.end : TextAlign.start,
+                  style: isMe
+                      ? Theme.of(context).textTheme.bodyText1
+                      : Theme.of(context).textTheme.bodyText2,
+                ),
+              ),
+              Text(
+                username,
+                style: Theme.of(context).textTheme.headline5,
+                textAlign: isMe ? TextAlign.end : TextAlign.start,
+              )
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
@@ -172,12 +228,10 @@ class DetailScreen extends StatelessWidget {
   final int index;
   final String imageUrl;
 
-
   DetailScreen(this.index, this.imageUrl);
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: GestureDetector(
         child: Center(
